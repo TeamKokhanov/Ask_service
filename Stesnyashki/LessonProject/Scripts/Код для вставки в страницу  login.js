3b11203908b5bd41a578f9e0c3028809ad014862
Evgeniy Danilenko <tyzdan@gmail.com>
2014-11-10 00:51:37 +0200

    Login VK and Facebook
    Логинизация через вк и фейсбук

---
+++ Stesnyashki/LessonProject/Scripts/Код для вставки в страницу  login.js
@@ -0,0 +1,2 @@
+<script src="//ulogin.ru/js/ulogin.js"></script>
+<div id="uLogin" data-ulogin="display=panel;fields=first_name,last_name;providers=vkontakte,facebook;hidden=;redirect_uri=http%3A%2F%2Fstesnyashki.com"></div>

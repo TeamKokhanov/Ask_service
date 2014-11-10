eb43267bf7bb7e9526e4e5df5eca42aa73f60b84
Survivon <eremenk.dima@gmail.com>
2014-11-10 19:43:41 +0200

    DB conection

---
+++ Stesnyashki/LessonProject/bin/StesnyashkiDB/StesnyashkiDB/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [email] VARCHAR(50) NOT NULL, 
+    [password] NVARCHAR(50) NOT NULL, 
+    [name] NVARCHAR(50) NOT NULL, 
+    [surname] NVARCHAR(50) NOT NULL, 
+    [age] INT NOT NULL, 
+    [sex] BIT NOT NULL, 
+    [country] NVARCHAR(50) NULL, 
+    [friendList] NVARCHAR(MAX) NULL, 
+    [followList] NVARCHAR(MAX) NULL
+)

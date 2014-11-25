53c0debad6fd613d2f0fa4adf60d242d0ade44a2
Survivon <eremenk.dima@gmail.com>
2014-11-25 20:41:48 +0200

    all task complite

---
+++ Stesnyashki/LessonProject/Questions/OuestionList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Stesnyashki.Models;
+using Stesnyashki.DAL;
+using System.Data;
+
+namespace Stesnyashki.Questions
+{
+    public class OuestionList
+    {
+        SQLConnector SQ = new SQLConnector();
+        public List<Question> QuestionListRetern(int idReciever) 
+        {
+           
+            DataTable QuestionList = SQ.Select("Question", "*", "idReciever=" + Convert.ToString(idReciever));
+            List<int> idSender = QuestionList.AsEnumerable().Select(r => r.Field<int>("idSender")).ToList();
+            List<string> qText = QuestionList.AsEnumerable().Select(r => r.Field<string>("qText")).ToList();
+            List<DateTime> qDate = QuestionList.AsEnumerable().Select(r => r.Field<DateTime>("qDate")).ToList();
+            List<int> id = QuestionList.AsEnumerable().Select(r => r.Field<int>("id")).ToList();
+            int j = 0;
+            List<Question> QuestionsList = new List<Question>();
+            foreach (var i in qText) 
+            {
+                Question q = new Question { id = id[j], idSender = idSender[j], idResiver = idReciever, qText = i, qDate = qDate[j] };
+                QuestionsList.Add(q);
+            }
+            return QuestionsList;
+        }
+
+        public bool Answer(int idQuestion,string answertext) 
+        {
+            List<string> arg = new List<string>();
+            List<string> values= new List<string>();
+            arg.Add(answertext);
+            arg.Add(Convert.ToString(DateTime.Now));
+            values.Add("aText");
+            values.Add("aDate");
+            try
+            {
+                SQ.Update("Question", arg, values, "id=" + Convert.ToString(idQuestion));
+                return true;
+            }
+            catch (Exception e) 
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

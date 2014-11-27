25985d3c1bfca5b9f7f4650bfcdde8538f554832
ValeraTkachev <valerkotkachov@gmail.com>
2014-11-27 19:51:16 +0200

    Add my Tasks as Controllers
    Add Update User Info into UserController,Create new
    controller(WallController),with methods that count statistics

--- Stesnyashki/LessonProject/Controllers/SUserController.cs
+++ Stesnyashki/LessonProject/Controllers/SUserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Stesnyashki.Models;
 
 namespace Stesnyashki.Controllers
 {
@@ -33,6 +34,162 @@ namespace Stesnyashki.Controllers
             else
                 ViewBag.pass = "Passwords mismaatch!!!";
             return View("User");
+        }
+
+        [HttpPost]
+        public ActionResult UpdateBg(int id, string newbg)//метод для update background в профиле пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                User u = Sh.Users.Find(id);
+                if (u == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    u.backgroundImage = newbg;
+
+                }
+            }
+            return View(User);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateMude(int id, string mude)//метод для update статуса в профиле пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                User u = Sh.Users.Find(id);
+                if (u == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    // u.mude = mude;
+
+                }
+            }
+            return View(User);
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePersonalInfo(int id, List<string> persinfo)//метод для update персональной информации в профиле пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                User u = Sh.Users.Find(id);
+                if (u == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    u.name = persinfo[0];
+                    u.surname = persinfo[1];
+                    u.age = Convert.ToInt32(persinfo[2]);
+                    u.sex = Convert.ToBoolean(persinfo[3]);
+                    u.country = persinfo[4];
+                    u.email = persinfo[5];
+                    u.password = persinfo[6];
+                    u.avatar = persinfo[7];
+                    u.isDataOpened = Convert.ToBoolean(persinfo[8]);
+                }
+
+            }
+
+            return View(User);
+        }
+
+
+        [HttpPost]
+        public ActionResult GetPrivacy(int id)//метод для получения информация  о статусе профиля пользователя(открыт или закрыт для просмотра) made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+            bool b;
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                User u = Sh.Users.Find(id);
+                if (u == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    b = Convert.ToBoolean(u.isDataOpened);
+                    if (b == true)
+                    {
+                        return View(User);
+                    }
+                    else
+                    {
+                        return HttpNotFound();
+                    }
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteUser(int adminId, int id)//метод для удаления пользователя админом made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+            if (adminId == -1)
+            {
+
+                if (id == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+
+                    foreach (Question q in Sh.Questions)
+                    {
+                        if (q.idResiver == id)
+                        {
+                            Sh.Questions.Remove(q);
+                            Sh.SaveChanges();
+                        }
+                    }
+                    User u = Sh.Users.Find(id);
+                    if (u != null)
+                    {
+                        Sh.Users.Remove(u);
+                        Sh.SaveChanges();
+                    }
+                }
+
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            return View(User);
+
         }
     }
 }
---
+++ Stesnyashki/LessonProject/Controllers/SWallController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Stesnyashki.Models;
+
+namespace Stesnyashki.Controllers
+{
+    public class SWallController : Controller
+    {
+        //
+        // GET: /SWall/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string CountLikes(int id)//метод для для подсчета кол-ва лайков пользователя находится на стене пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+            int sum = 0;
+            if (id == null)
+            {
+                return "User with this id isn`t exist";
+            }
+            else
+            {
+                foreach (Question q in Sh.Questions)
+                {
+                    if (q.idResiver == id)
+                    {
+                        sum += Convert.ToInt32(q.like);
+                    }
+
+                }
+
+            }
+           
+            return sum.ToString();
+        }
+
+        [HttpPost]
+        public string CountQuestions(int id)//метод для для подсчета кол-ва вопросов заданных пользователю  находится на стене пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+
+            int sum = 0;
+            if (id == null)
+            {
+                return "User with this id isn`t exist";
+            }
+            else
+            {
+                foreach (Question q in Sh.Questions)
+                {
+                    if (q.idResiver == id)
+                    {
+                        sum++;
+                    }
+
+                }
+
+            }
+            return sum.ToString();
+        }
+
+
+        [HttpPost]
+        public string CountAnswers(int id)//метод для для подсчета кол-ва ответов пользователя находится на стене пользователя made by Valera
+        {
+            ShyMeContext Sh = new ShyMeContext();
+            int sum = 0;
+            if (id == null)
+            {
+                return "User with this id isn`t exist";
+            }
+            else
+            {
+                foreach (Question q in Sh.Questions)
+                {
+                    if (q.idResiver == id && q.aText != null)
+                    {
+                        sum++;
+                    }
+
+                }
+
+            }
+            return sum.ToString();
+        }
+    }
+}

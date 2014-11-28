d3943a2c8d24905acdf923f2f8596d742d70eefe
Evgeniy Danilenko <tyzdan@gmail.com>
2014-11-28 16:26:47 +0200

    авторизация через соц сети

--- Stesnyashki/LessonProject/App_Start/RouteConfig.cs
+++ Stesnyashki/LessonProject/App_Start/RouteConfig.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Stesnyashki.Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using  Microsoft.Web.WebPages.OAuth;
 
 namespace Stesnyashki
 {
@@ -25,6 +27,13 @@ namespace Stesnyashki
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "SHome", action = "Index", id = UrlParameter.Optional }
             );
+
+            OAuthWebSecurity.RegisterClient(
+       client: new VKontakteAuthenticationClient(
+              "4649158", "VqQe594IUCLxZb9gQ09B"),
+       displayName: "ВКонтакте", // надпись на кнопке
+       extraData: null);
+
         }
     }
 }
